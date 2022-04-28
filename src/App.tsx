9e5ace2d4b6a05ccfced6eaa5aaa33aadef257d4
alfonsoaranzazu <alfonso@viruett.com>
2022-04-28 15:49:35 -0700

    adjust background image

--- src/App.tsx
+++ src/App.tsx
@@ -46,9 +46,9 @@ const useStyles: any = makeStyles((theme: Theme) =>
       backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(0, 0, 0, 0.1)), url(${stakingBackground})`,
       backgroundColor: '#000000',
       backgroundRepeat: 'no-repeat',
-      backgroundSize: 'contain',
-      [theme.breakpoints.down('md')]: {
-        backgroundSize: 'cover',
+      backgroundSize: 'cover',
+      [theme.breakpoints.down('xl')]: {
+        backgroundSize: 'contain',
       }
     },
     avatar: {
