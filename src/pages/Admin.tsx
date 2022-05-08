7d9f37395dacfc08b5a6b69ce901aa620d3e031e
alfonsoaranzazu <alfonso@viruett.com>
2022-05-08 12:22:35 -0700

    text field styling

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,5 +1,6 @@
 import { Grid, TextField, Typography, createStyles, Theme } from "@mui/material";
 import { makeStyles } from '@mui/styles';
+import { ChangeEvent, useEffect, useState } from "react";
 
 const useStyles: any = makeStyles((theme: Theme) => 
   createStyles({
@@ -11,14 +12,28 @@ const useStyles: any = makeStyles((theme: Theme) =>
 
 export default function Admin() {
   const classes = useStyles();
+  const [stakingWallet, setStakingWallet] = useState("");
+
+  const onStakingWalletChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setStakingWallet(event.target.value);
+  }
+
+  useEffect(() => {
+    if (stakingWallet) {
+      console.log("staking wallet change: ", stakingWallet);
+    }
+  }, [stakingWallet])
 
   return (
     <Grid id="admin" container alignItems="center" direction="column" pt={10} pb={30}>
       <Typography variant="h3" pb={10}>Admin Page </Typography>
 
       <Grid id="staker" justifyContent="center" container>
-        <Grid item xl={4} lg={12}>
-          <TextField id="wallet" label="Staking Wallet" variant="outlined" fullWidth />
+        <Grid item xl={4} lg={4} md={6} sm={8} xs={10}>
+          <TextField id="wallet" label="Staking Wallet" variant="outlined" fullWidth value={stakingWallet} onChange={onStakingWalletChange}/>
+        </Grid>
+        <Grid item>
+
         </Grid>
       </Grid>
     </Grid>
