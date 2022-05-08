bf3add3e879091cfb3e4772a1969e3a8bc29874a
alfonsoaranzazu <alfonso@viruett.com>
2022-05-08 12:17:21 -0700

    adds staking wallet input

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,10 +1,26 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, TextField, Typography, createStyles, Theme } from "@mui/material";
+import { makeStyles } from '@mui/styles';
 
+const useStyles: any = makeStyles((theme: Theme) => 
+  createStyles({
+    walletInput: {
+      
+    },
+  })
+);
 
 export default function Admin() {
+  const classes = useStyles();
+
   return (
-    <Grid id="admin" container alignItems="center" direction="column" pt={30} pb={30}>
-      <Typography variant="h1">Admin Page Coming Soon</Typography>
+    <Grid id="admin" container alignItems="center" direction="column" pt={10} pb={30}>
+      <Typography variant="h3" pb={10}>Admin Page </Typography>
+
+      <Grid id="staker" justifyContent="center" container>
+        <Grid item xl={4} lg={12}>
+          <TextField id="wallet" label="Staking Wallet" variant="outlined" fullWidth />
+        </Grid>
+      </Grid>
     </Grid>
   )
 }
