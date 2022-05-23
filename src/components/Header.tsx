4e6680bc348035994a0cc7cded5439e6fb86bb78
alfonsoaranzazu <alfonso@viruett.com>
2022-05-22 18:55:48 -0700

    style appbar for desktop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -40,20 +40,26 @@ export default function Header() {
     } else if (location.pathname === "/admin") {
       return "Froggy Admin"
     }
-      
+  }
+
+  const onCartClick = () => {
+    navigate("/market");
+
+    // show items modal
+
   }
 
   return (
       <AppBar position="fixed">
-        <Toolbar>
-          <Grid container item justifyContent='center' xl={4} lg={4} md={3} sm={3} xs={3}>
-            <Link href={REACT_APP_WEBSITE_URL} underline='none'>
-              <Avatar className={classes.avatar} alt='Home' src={logo} sx={{width: 75, height: 75}}/>
-            </Link>
-            <Typography variant='h4' color='secondary' fontWeight='bold' alignSelf="center" pl={3}>{getTitle()}</Typography>
-          </Grid>
-          { isDesktop && 
-            <Grid container item justifyContent='center' alignItems="center" textAlign='center' xl={6} lg={6} md={9} sm={9} xs={9}>
+        <Toolbar disableGutters>
+          <Grid container justifyContent="space-between" xl={12} lg={12} md={12} sm={12} xs={12}>
+            <Grid container item justifyContent='center' xl={3} lg={3} md={4} sm={4} xs={3}>
+              <Link href={REACT_APP_WEBSITE_URL} underline='none'>
+                <Avatar className={classes.avatar} alt='Home' src={logo} sx={{width: 75, height: 75}}/>
+              </Link>
+              <Typography variant='h5' color='secondary' fontWeight='bold' alignSelf="center" pl={3}>{getTitle()}</Typography>
+            </Grid>
+            <Grid container item justifyContent='center' alignItems="center" textAlign='center' xl={6} lg={6} md={6} sm={6} xs={9}>
               <Grid item xl={2} lg={2} md={2} sm={2} xs={2}>
                 <Link href={REACT_APP_WEBSITE_URL + '/team'} underline='none' variant="h5" color="secondary">Team</Link>
               </Grid>
@@ -70,13 +76,13 @@ export default function Header() {
                 <Link href={REACT_APP_WEBSITE_URL + '/license'} underline='none' variant="h5" color="secondary">License</Link>
               </Grid> 
             </Grid>
-          }
-          <Grid item alignSelf="center">
-            <Fab size='medium'>
-              <Badge badgeContent={cartItemCount} color="primary">
-                <ShoppingCartIcon fontSize='large'/>
-              </Badge>
-            </Fab>
+            <Grid container item justifyContent="center" alignItems="center" xl={1} lg={1} md={1} sm={1} xs={1}>
+              <Fab size='medium' onClick={onCartClick}>
+                <Badge badgeContent={cartItemCount} color="primary">
+                  <ShoppingCartIcon fontSize='large'/>
+                </Badge>
+              </Fab>
+            </Grid>
           </Grid>
         </Toolbar>
       </AppBar>
