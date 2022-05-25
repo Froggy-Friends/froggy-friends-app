14b8d7a632daff7bc8db8b7c066b40115c8a4915
alfonsoaranzazu <alfonso@viruett.com>
2022-05-25 01:05:03 -0700

    add hamburger icon on mobile header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import { Grid, Avatar, Link, createStyles, Theme, useMediaQuery, Typography, Badge, Fab, AppBar, Toolbar } from "@mui/material";
+import { Grid, Avatar, Link, createStyles, Theme, useMediaQuery, Typography, Badge, Fab, AppBar, Toolbar, IconButton } from "@mui/material";
 import { makeStyles } from '@mui/styles';
 import logo from '../images/logo.png';
 import theme from "../theme";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MenuIcon from '@mui/icons-material/Menu';
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { cartCount, toggle } from "../redux/cartSlice";
 
@@ -31,7 +32,7 @@ export default function Header() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const location = useLocation();
-  const isDesktop = useMediaQuery(theme.breakpoints.up('sm'));
+  const isMobile = useMediaQuery(theme.breakpoints.down('lg'));
 
   const getTitle = () => {
     if (location.pathname === "/market") {
@@ -41,7 +42,7 @@ export default function Header() {
     } else if (location.pathname === "/admin") {
       return "Froggy Admin"
     } else if (location.pathname === "/leaderboard") {
-      return "Ribbit Leaderboard";
+      return "Leaderboard";
     }
   }
 
@@ -54,14 +55,14 @@ export default function Header() {
   return (
       <AppBar position="fixed">
         <Toolbar disableGutters>
-          <Grid container justifyContent="space-between" xl={12} lg={12} md={12} sm={12} xs={12}>
-            <Grid container item justifyContent='center' xl={3} lg={3} md={4} sm={4} xs={3}>
+          <Grid id="header" container justifyContent="space-between" alignItems="center" pl={2} pr={2} xl={12} lg={12} md={12} sm={12} xs={12}>
+            <Grid container item xl={3} lg={4} md={4} sm={10} xs={10}>
               <Link href={REACT_APP_WEBSITE_URL} underline='none'>
-                <Avatar className={classes.avatar} alt='Home' src={logo} sx={{width: 75, height: 75}}/>
+                <Avatar className={classes.avatar} alt='Home' src={logo} sx={{width: 65, height: 65}}/>
               </Link>
               <Typography variant='h5' color='secondary' fontWeight='bold' alignSelf="center" pl={3}>{getTitle()}</Typography>
             </Grid>
-            <Grid container item justifyContent='center' alignItems="center" textAlign='center' pr={3} xl={6} lg={6} md={6} sm={6} xs={9}>
+            <Grid container item display={isMobile ? "none" : "flex"} xl={6} lg={6} mx={8} sm={12} xs={12}>
               <Grid item xl={2} lg={2} md={2} sm={2} xs={2}>
                 <Link href={REACT_APP_WEBSITE_URL + '/team'} underline='none' variant="h5" color="secondary">Team</Link>
               </Grid>
@@ -80,6 +81,11 @@ export default function Header() {
               <Grid item xl={2} lg={2} md={2} sm={2} xs={2}>
                 <Link href={REACT_APP_WEBSITE_URL + '/license'} underline='none' variant="h5" color="secondary">License</Link>
               </Grid> 
+            </Grid>
+            <Grid item display={isMobile ? "flex" : "none"} textAlign="end" pr={3} sm={2} xs={2}>
+              <IconButton size="large" color="inherit" aria-label="menu">
+                <MenuIcon/>
+              </IconButton>
             </Grid>
             {/* <Grid container item justifyContent="center" alignItems="center" xl={1} lg={1} md={1} sm={1} xs={1}>
               <Fab size='medium' onClick={onCartClick}>
--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -4,13 +4,12 @@ import skyscrapers from "../images/skyscrapers.png";
 import { useEffect, useState } from 'react';
 import { Leaderboard } from '../models/Leaderboard';
 import axios from 'axios';
-
+import ribbit from '../images/ribbit.gif';
 
 const useStyles: any = makeStyles((theme: Theme) => 
   createStyles({
     leaderboard: {
       backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0, 0, 0, 0)), url(${skyscrapers})`,
-      backgroundColor: '#000000',
       backgroundRepeat: 'no-repeat',
       backgroundSize: 'cover',
       minHeight: '110%',
@@ -74,7 +73,10 @@ export default function Board() {
                     <Typography variant='h6' color='secondary'>{leader.account}</Typography>
                   </TableCell>
                   <TableCell>
-                    <Typography variant='h6' color='secondary'>{leader.ribbit}</Typography>
+                    <Grid container alignItems="center" minWidth={200}>
+                      <img src={ribbit} style={{height: 25, width: 25}} alt='ribbit'/>
+                      <Typography variant='h6' color='secondary' pl={1}>{leader.ribbit}</Typography>
+                    </Grid>
                   </TableCell>
                 </TableRow>
               ))}
