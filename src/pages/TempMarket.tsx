23e298a7a308c975136cd14c6a10f675ad7856dc
alfonsoaranzazu <alfonso@viruett.com>
2022-05-24 22:47:26 -0700

    updates coming soon

--- src/pages/TempMarket.tsx
+++ src/pages/TempMarket.tsx
@@ -1,11 +1,12 @@
 import { createStyles, Grid, Theme, Typography } from "@mui/material";
 import { makeStyles } from '@mui/styles';
 import market from "../images/market.png";
+import biz from '../images/biz.png';
 
 const useStyles: any = makeStyles((theme: Theme) => 
   createStyles({
     market: {
-      backgroundImage: `linear-gradient(rgba(0,0,0,0.4), rgba(0, 0, 0, 0)), url(${market})`,
+      backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0, 0, 0, 0)), url(${market})`,
       backgroundColor: '#000000',
       backgroundRepeat: 'no-repeat',
       backgroundSize: 'cover',
@@ -18,8 +19,9 @@ export default function TempMarket() {
   const classes = useStyles();
 
   return (
-    <Grid id="market" className={classes.market} container justifyContent="center" pt={20} pb={30}>
-      <Typography variant='h2' fontWeight="bold" color='secondary'>Coming Soon</Typography>
+    <Grid id="market" className={classes.market} container direction="column" justifyContent="center" alignItems="center" pb={30}>
+      <Typography variant='h2' fontWeight="bold" color='secondary' pb={5}>Coming Soon</Typography>
+      <img src={biz} alt="Coming Soon" style={{height: 200, width: 200}}/>
     </Grid>
   )
 }
