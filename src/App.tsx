9a58f0c7ad356b243a565aaa89216e0c69e365d2
alfonsoaranzazu <alfonso@viruett.com>
2022-05-26 22:37:22 -0700

    remove temp market

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import Staking from "./pages/Staking";
 import Market from "./pages/Market";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import MusicPlayer from "./components/MusicPlayer";
-import TempMarket from "./pages/TempMarket";
 import Board from "./pages/Board";
 
 export default function App() {
@@ -16,12 +14,11 @@ export default function App() {
       <Routes>
         <Route path="/staking" element={<Staking/>} />
         <Route path="/admin" element={<Admin/>} />
-        <Route path="/market" element={<TempMarket/>}/>
+        <Route path="/market" element={<Market/>}/>
         <Route path="/leaderboard" element={<Board/>} />
         <Route path="*" element={ <Navigate to="/staking" replace />} />
       </Routes>
       <Footer/>
-      {/* <MusicPlayer/> */}
     </Fragment>
   )
 }
