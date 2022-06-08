40d3a87d2f9b7b24541afd08fa9b8c6ef9d07569
alfonsoaranzazu <alfonso@viruett.com>
2022-06-07 23:03:58 -0700

    style cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -11,7 +11,6 @@ import { useSpendingApproved, useApproveSpender, useBundleBuy } from '../client'
 import { Check, Close, Warning } from '@mui/icons-material';
 import CancelIcon from '@mui/icons-material/Cancel';
 import ribbit from '../images/ribbit.gif';
-import useWindowDimensions from '../hooks/useWindowDimensions';
 import please from '../images/plz.png';
 import hype from '../images/hype.png';
 import uhhh from '../images/uhhh.png';
@@ -23,12 +22,10 @@ const useStyles: any = makeStyles((theme: Theme) =>
       top: '50%',
       left: '50%',
       transform: 'translate(-50%, -50%)',
-      width: 400,
       backgroundColor: theme.palette.common.white,
-      border: '2px solid #000',
+      borderRadius: 5,
       boxShadow: 24,
       p: 4,
-      minHeight: 500,
       [theme.breakpoints.down('md')]: {
         top: '0%',
         left: '0%',
@@ -63,7 +60,6 @@ const useStyles: any = makeStyles((theme: Theme) =>
 
 export default function Cart() {
   const classes = useStyles();
-  const { height } = useWindowDimensions();
   const dispatch = useAppDispatch();
   const isCartOpen = useAppSelector(cartOpen);
   const items = useAppSelector(cartItems);
@@ -126,18 +122,6 @@ export default function Cart() {
       setRemaining(remaining);
     }
   }, [items]);
-
-  const getItemsHeight = () => {
-    if (height < 500) {
-      return 200;
-    } else if (height < 800) {
-      return 400;
-    } else if (height < 1000) {
-      return 500;
-    } else {
-      return 600;
-    }
-  }
 
   const onRemoveItem = (item: RibbitItem) => {
     dispatch(remove(item));
@@ -210,71 +194,71 @@ export default function Cart() {
           }}
         >
         <Fade id='cart' in={isCartOpen}>
-          <Grid className={classes.cart} container item direction="column" justifyContent='space-between' p={2} xl={3} lg={4} md={5} sm={12} xs={12}>
-            <Grid item id='title-and-items' xs={height < 500 ? 8 : 9}>
-              <Grid id='title' container item justifyContent='space-between' alignItems="center" pb={3}>
-                <Grid item>
-                  <Typography variant='h4' color='info' fontWeight='bold'>Ribbit Cart</Typography>
-                </Grid>
-                <Grid item>
-                  <IconButton size='large' color='info' onClick={handleClose}>
-                    <Close/>
-                  </IconButton>
-                </Grid>
+          <Grid className={classes.cart} item xl={4} lg={6} md={8} sm={12} xs={12} container direction="column" p={2}>
+            <Grid id='title' item xs={1} container justifyContent='space-between' alignItems='center' pb={2}>
+              <Grid item>
+                <Typography variant='h4' color='info' fontWeight='bold'>Ribbit Cart</Typography>
               </Grid>
-              <Grid id='cart-items' item minHeight={getItemsHeight} maxHeight={getItemsHeight} sx={{overflowY: 'scroll', "::-webkit-scrollbar": { backgroundColor: 'transparent'}}}>
-                {
-                  items.map((item, index) => {
-                    return <Grid className={classes.cartItem} key={index} container item mb={1} xl={12} lg={12} md={12} sm={12} xs={12}>
-                        <Grid id='item-image' item p={1} xl={2} lg={2} md={1} sm={1} xs={2}>
-                          <CardMedia component="img" image={item.image} alt={item.name}/>
-                        </Grid>
-                        <Grid id='item-title' item justifySelf="start" xl={5} lg={5} md={5} sm={5} xs={4}>
-                          <Typography variant='subtitle1' color='secondary' pl={2}>{item.name}</Typography>
-                        </Grid>
-                        <Grid item display='flex' p={1} xl={3} lg={3} md={4} sm={4} xs={4}>
-                          <img src={ribbit} style={{height: 25, width: 25}} alt='ribbit'/>
-                          <Typography variant='subtitle1'>{commify(item.price)}</Typography>
-                        </Grid>
-                        <Grid item textAlign='center' xl={2} lg={2} md={2} sm={2} xs={2}>
-                          <IconButton size='small' color='primary' onClick={() => onRemoveItem(item)}>
-                            <CancelIcon/>
-                          </IconButton>
-                        </Grid>
-                    </Grid>
-                  })
-                }
+              <Grid item>
+                <IconButton size='large' color='info' onClick={handleClose}>
+                  <Close/>
+                </IconButton>
               </Grid>
             </Grid>
-            <Grid item id='total-and-checkout' xs={height < 500 ? 4 : 3}>
-              <Grid id="total" item mr={1} p={1}>
-                <Grid className={classes.cartItem} container item justifyContent='space-between' xl={12} lg={12} md={12} sm={12} xs={12}>
-                  <Typography variant='h6' color='secondary' p={1} pl={2}>Ribbit balance</Typography>
-                  <Grid item display='flex' justifyContent='center' alignItems='center' p={1} pr={2}>
-                    <img src={ribbit} style={{height: 25, width: 25}} alt='ribbit'/>
-                    <Typography>{formatBalance(ribbitBalance)}</Typography>
+            <Grid id='items' item xs={7} maxHeight='50vh' overflow='scroll' 
+              sx={{
+                  "::-webkit-scrollbar": { width: 0, height: 0, backgroundColor: "transparent"}, 
+                  
+              }}>
+              {
+                items.map((item, index) => {
+                  return <Grid className={classes.cartItem} key={index} container item mb={1} xl={12} lg={12} md={12} sm={12} xs={12}>
+                      <Grid id='item-image' item p={1} xl={2} lg={2} md={1} sm={1} xs={2}>
+                        <CardMedia component="img" image={item.image} alt={item.name}/>
+                      </Grid>
+                      <Grid id='item-title' item justifySelf="start" xl={5} lg={5} md={5} sm={5} xs={4}>
+                        <Typography variant='subtitle1' color='secondary' pl={2}>{item.name}</Typography>
+                      </Grid>
+                      <Grid item display='flex' p={1} xl={3} lg={3} md={4} sm={4} xs={4}>
+                        <img src={ribbit} style={{height: 25, width: 25}} alt='ribbit'/>
+                        <Typography variant='subtitle1'>{commify(item.price)}</Typography>
+                      </Grid>
+                      <Grid item textAlign='center' xl={2} lg={2} md={2} sm={2} xs={2}>
+                        <IconButton size='small' color='primary' onClick={() => onRemoveItem(item)}>
+                          <CancelIcon/>
+                        </IconButton>
+                      </Grid>
                   </Grid>
-                </Grid>
-                <Grid className={classes.cartItem} container item justifyContent='space-between' xl={12} lg={12} md={12} sm={12} xs={12}>
-                  <Typography variant='h6' color='secondary' p={1} pl={2}>Cart total</Typography>
-                  <Grid item display='flex' justifyContent='center' alignItems='center' p={1} pr={2}>
-                    <img src={ribbit} style={{height: 25, width: 25}} alt='ribbit'/>
-                    <Typography>{commify(total.toFixed(2))}</Typography>
-                  </Grid>
-                </Grid>
-                <Grid className={classes.cartItem} container item justifyContent='space-between' xl={12} lg={12} md={12} sm={12} xs={12}>
-                  <Typography variant='h6' color='secondary' p={1} pl={2}>Remaining</Typography>
-                  <Grid item display='flex' justifyContent='center' alignItems='center' p={1} pr={2}>
-                    <img src={ribbit} style={{height: 25, width: 25}} alt='ribbit'/>
-                    <Typography>{commify(remaining.toFixed(2))}</Typography>
-                  </Grid>
+                })
+              }
+            </Grid>
+            <Grid id="total" item xs={3} pt={2}>
+              <Grid className={classes.cartItem} container item justifyContent='space-between' xl={12} lg={12} md={12} sm={12} xs={12}>
+                <Typography variant='h6' color='secondary' p={1} pl={2}>Ribbit</Typography>
+                <Grid item display='flex' justifyContent='center' alignItems='center' p={1} pr={2}>
+                  <img src={ribbit} style={{height: 25, width: 25}} alt='ribbit'/>
+                  <Typography>{formatBalance(ribbitBalance)}</Typography>
                 </Grid>
               </Grid>
-              <Grid id='checkout' container item justifyContent='center' p={1}>
-                <Button variant='contained' color='success' fullWidth disabled={isCheckoutDisabled()} onClick={checkout}>
-                  <Typography variant='subtitle1' color='secondary'>Checkout</Typography>
-                </Button>
+              <Grid className={classes.cartItem} container item justifyContent='space-between' xl={12} lg={12} md={12} sm={12} xs={12}>
+                <Typography variant='h6' color='secondary' p={1} pl={2}>Cart total</Typography>
+                <Grid item display='flex' justifyContent='center' alignItems='center' p={1} pr={2}>
+                  <img src={ribbit} style={{height: 25, width: 25}} alt='ribbit'/>
+                  <Typography>{commify(total.toFixed(2))}</Typography>
+                </Grid>
               </Grid>
+              <Grid className={classes.cartItem} container item justifyContent='space-between' xl={12} lg={12} md={12} sm={12} xs={12}>
+                <Typography variant='h6' color='secondary' p={1} pl={2}>Remaining</Typography>
+                <Grid item display='flex' justifyContent='center' alignItems='center' p={1} pr={2}>
+                  <img src={ribbit} style={{height: 25, width: 25}} alt='ribbit'/>
+                  <Typography>{commify(remaining.toFixed(2))}</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid id='checkout' item xs={1} pt={2}>
+              <Button variant='contained' color='success' fullWidth disabled={isCheckoutDisabled()} onClick={checkout}>
+                <Typography variant='subtitle1' color='secondary'>Checkout</Typography>
+              </Button>
             </Grid>
           </Grid>
         </Fade> 
