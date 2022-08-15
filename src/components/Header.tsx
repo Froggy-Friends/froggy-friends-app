26cb2823411a7d62d0d6e0ebbb83688c1a57163d
alfonsoaranzazu <alfonso@viruett.com>
2022-08-14 20:17:27 -0700

    adds uniswap widget

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,12 @@ import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { shortenAddress, useEthers, useLookupAddress } from "@usedapp/core";
 import { makeStyles } from '@mui/styles';
 import { Grid, Avatar, Link, createStyles, Theme, useMediaQuery, Typography, Badge, Fab, AppBar, Toolbar, IconButton, Drawer, Fade, Button } from "@mui/material";
-import { Close, ShoppingCart, Menu, Headphones } from "@mui/icons-material";
+import { Close, ShoppingCart, Menu, Headphones, MonetizationOn } from "@mui/icons-material";
 import { cartCount, toggle } from "../redux/cartSlice";
 import { isPlaying } from "../redux/musicSlice";
+import { darkTheme, SwapWidget } from '@uniswap/widgets'
+import { uniswapConfig } from '../config';
+import '@uniswap/widgets/fonts.css'
 import logo from '../images/logo.png';
 import theme from "../theme";
 import MusicPlayer from "./MusicPlayer";
@@ -31,6 +34,14 @@ const useStyles: any = makeStyles((theme: Theme) =>
       position: 'absolute',
       inset: '80px 10px auto auto',
       maxWidth: 350
+    },
+    swap: {
+      position: 'absolute' as 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      padding: 4,
+      justifyContent: 'center'
     }
   })
 );
@@ -48,6 +59,7 @@ export default function Header() {
   const isMarket = location.pathname === "/market";
   const [sidemenuOpen, setSidemenuOpen] = useState<boolean>(false);
   const [musicOpen, setMusicOpen] = useState<boolean>(false);
+  const [swapOpen, setSwapOpen] = useState<boolean>(false);
   const { activateBrowserWallet, account } = useEthers();
   const ens = useLookupAddress();
   const [displayName, setDisplayName] = useState<string>("");
@@ -70,6 +82,10 @@ export default function Header() {
     } else if (location.pathname === "/leaderboard") {
       return "Leaderboard";
     }
+  }
+
+  const onSwapClick = () => {
+    setSwapOpen(!swapOpen);
   }
 
   const onMusicClick = () => {
@@ -111,6 +127,11 @@ export default function Header() {
                 </Grid> 
               </Grid>
               <Grid id='buttons' container item justifyContent="end" alignItems='center' p={1} xl={3} lg={3} md={3} sm={6} xs={10}>
+                <Grid item display="flex" pr={2}>
+                  <Fab size='small' onClick={onSwapClick}>
+                    <MonetizationOn color={swapOpen ? "primary" : "inherit"} fontSize="medium"/>
+                  </Fab>
+                </Grid>
                 <Grid item display="flex" pr={2}>
                   <Fab size='small' onClick={onMusicClick}>
                     <Badge invisible={!playing} badgeContent=" " color="primary">
@@ -185,6 +206,17 @@ export default function Header() {
         <Fade in={musicOpen}>
           <Grid id='player' className={classes.musicPlayer} container zIndex={1}>
             <MusicPlayer inverted={false}/>
+          </Grid>
+        </Fade>
+        <Fade in={swapOpen}>
+          <Grid id='swap' className={classes.swap} container zIndex={1}>
+            <SwapWidget 
+              theme={darkTheme} 
+              jsonRpcUrlMap={uniswapConfig.jsonRpcUrlMap}
+              tokenList={uniswapConfig.tokenList}  
+              defaultOutputAmount={uniswapConfig.outputAmount}
+              defaultOutputTokenAddress={uniswapConfig.outputTokenAddress}
+            />
           </Grid>
         </Fade>
         <Cart />
