b5ac7ab8d8eaf21f1c893932e0d8957778a56cb7
Alfonso <alfonso@viruett.com>
2022-09-18 17:21:39 -0700

    adds frog details skeleton

--- src/pages/FrogDetails.tsx
+++ src/pages/FrogDetails.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { ArrowBack, Close } from "@mui/icons-material";
-import { Button, Chip, Container, Grid, IconButton, Snackbar, SnackbarContent, Stack, Typography, useMediaQuery, useTheme, Paper } from "@mui/material";
+import { Button, Chip, Container, Grid, IconButton, Snackbar, SnackbarContent, Stack, Typography, useMediaQuery, useTheme, Paper, Skeleton } from "@mui/material";
 import { Froggy } from "../models/Froggy";
 import { useEthers } from "@usedapp/core";
 import { useStakingDeposits } from '../client';
@@ -76,11 +76,23 @@ export default function FrogDetails() {
             <Container maxWidth='lg'>
                 <Grid id='top-row' container justifyContent='space-between' pb={10}>
                     <Grid id='image' item xl={4} lg={4} md={5} sm={5} xs={12} pb={isXs ? 5 : 0}>
-                        <img src={frog?.image} width='100%' style={{borderRadius: 5}}/>
+                        {
+                            frog ? (
+                                <img src={frog?.image} width='100%' style={{borderRadius: 5}}/>
+                            ) : (
+                                <Skeleton variant='rectangular' animation='wave' height={350}/>
+                            )
+                        }
                     </Grid>
                     <Grid id='info' container item direction='column' justifyContent='space-between' minHeight={isSm ? 400 : 'inherit'} xl={7} lg={7} md={6} sm={6} xs={12}>
                         <Grid id='title-and-exit' container justifyContent='space-between' alignItems='center'>
-                            <Typography variant='h5' fontWeight='bold'>{frog?.name}</Typography>
+                            {
+                                frog ? (
+                                    <Typography variant='h5' fontWeight='bold'>{frog?.name}</Typography>
+                                ) : (
+                                    <Skeleton variant='text' animation='wave' height={35} width={200}/>
+                                )
+                            }
                             <Paper elevation={3} sx={{borderRadius: 25}}>
                                 <IconButton className="cta" onClick={onItemDetailsClose}>   
                                     <ArrowBack/>
@@ -123,7 +135,7 @@ export default function FrogDetails() {
                         <Stack id='tags' spacing={1}>
                             <Typography fontWeight='bold'>Tags</Typography>
                             <Grid container spacing={2}>
-                                <Grid item ml={-2}><Chip label={frog?.rarity}/></Grid>
+                                <Grid item ml={-2}><Chip label={frog?.rarity || 'common'}/></Grid>
                                 <Grid item><Chip label={frog?.isStaked ? 'staked' : 'unstaked'}/></Grid>
                                 <Grid item><Chip label={frog?.isPaired ? 'paired' : 'unpaired'}/></Grid>
                             </Grid>
@@ -138,51 +150,55 @@ export default function FrogDetails() {
                     </Grid>
                 </Grid>
                 <Grid id='bottom-row' container justifyContent='space-between'>
-                    <Grid id='traits' container item xl={4} lg={4} md={5} sm={5} xs={12} pb={10}>
-                        <Stack spacing={3}>
-                            <Typography variant='h5' fontWeight='bold'>Traits</Typography>
-                            <Grid container>
-                                {
-                                    frog?.attributes.map(trait => {
-                                        return (
-                                            <Grid item xl={4} lg={4} md={4} sm={6} xs={6} pb={3}>
-                                                <Typography fontWeight='bold'>{trait.trait_type}</Typography>
-                                                <Typography>{trait.value}</Typography>
-                                            </Grid>
-                                        )
-                                    })
-                                }
-                            </Grid>
-                        </Stack>
-                    </Grid>
-                    <Grid id='download' item xl={7} lg={7} md={6} sm={6} xs={12} pb={5}>
-                        <Stack spacing={3}>
-                            <Typography variant='h5' fontWeight='bold'>Download Assets</Typography>
-                            <Stack direction={isXs ? 'column' : 'row'} spacing={3}>
-                                {
-                                    frog?.image && 
-                                    <Stack alignItems='center' spacing={5} pb={5}>
-                                        <img src={frog.image} width={isSm ? '100%' : 200} style={{borderRadius: 5}}/>
-                                        <Button variant='contained' color="primary" sx={{height: 50, width: 130}}onClick={() => downloadAsset(frog.image, `${frog.edition}.png`)}>Download</Button>
-                                    </Stack>
-                                }
-                                {
-                                    frog?.imagePixel && 
-                                    <Stack alignItems='center' spacing={5} pb={5}>
-                                        <img src={frog?.imagePixel} width={isSm ? '100%' : 200} style={{borderRadius: 5}}/>
-                                        <Button variant='contained' color="primary" sx={{height: 50, width: 130}} onClick={() => downloadAsset(frog.imagePixel, `${frog.edition}-pixel.png`)}>Download</Button>
-                                    </Stack>
-                                }
-                                {
-                                    frog?.image3d && 
-                                    <Stack alignItems='center' spacing={5} pb={5}>
-                                        <img src={frog.image3d} width={isSm ? '100%' : 200} style={{borderRadius: 5}}/>
-                                        <Button variant='contained' color="primary" sx={{height: 50, width: 130}} onClick={() => downloadAsset(frog.image3d, `${frog.edition}-3d.png`)}>Download</Button>
-                                    </Stack>
-                                }
+                    {
+                        frog && <Grid id='traits' container item xl={4} lg={4} md={5} sm={5} xs={12} pb={10}>
+                            <Stack spacing={3}>
+                                <Typography variant='h5' fontWeight='bold'>Traits</Typography>
+                                <Grid container>
+                                    {
+                                        frog.attributes.map(trait => {
+                                            return (
+                                                <Grid item xl={4} lg={4} md={4} sm={6} xs={6} pb={3}>
+                                                    <Typography fontWeight='bold'>{trait.trait_type}</Typography>
+                                                    <Typography>{trait.value}</Typography>
+                                                </Grid>
+                                            )
+                                        })
+                                    }
+                                </Grid>
                             </Stack>
-                        </Stack>
-                    </Grid>
+                        </Grid>
+                    }
+                    {
+                        frog && <Grid id='download' item xl={7} lg={7} md={6} sm={6} xs={12} pb={5}>
+                            <Stack spacing={3}>
+                                <Typography variant='h5' fontWeight='bold'>Download Assets</Typography>
+                                <Stack direction={isXs ? 'column' : 'row'} spacing={3}>
+                                    {
+                                        frog.image && 
+                                        <Stack alignItems='center' spacing={5} pb={5}>
+                                            <img src={frog.image} width={isSm ? '100%' : 200} style={{borderRadius: 5}}/>
+                                            <Button variant='contained' color="primary" sx={{height: 50, width: 130}}onClick={() => downloadAsset(frog.image, `${frog.edition}.png`)}>Download</Button>
+                                        </Stack>
+                                    }
+                                    {
+                                        frog.imagePixel && 
+                                        <Stack alignItems='center' spacing={5} pb={5}>
+                                            <img src={frog.imagePixel} width={isSm ? '100%' : 200} style={{borderRadius: 5}}/>
+                                            <Button variant='contained' color="primary" sx={{height: 50, width: 130}} onClick={() => downloadAsset(frog.imagePixel, `${frog.edition}-pixel.png`)}>Download</Button>
+                                        </Stack>
+                                    }
+                                    {
+                                        frog.image3d && 
+                                        <Stack alignItems='center' spacing={5} pb={5}>
+                                            <img src={frog.image3d} width={isSm ? '100%' : 200} style={{borderRadius: 5}}/>
+                                            <Button variant='contained' color="primary" sx={{height: 50, width: 130}} onClick={() => downloadAsset(frog.image3d, `${frog.edition}-3d.png`)}>Download</Button>
+                                        </Stack>
+                                    }
+                                </Stack>
+                            </Stack>
+                        </Grid>
+                    }
                 </Grid>
             </Container>
             <Snackbar open={showAlert}  autoHideDuration={5000} message={alertMessage} anchorOrigin={{ vertical: 'top', horizontal: 'center' }} onClose={onAlertClose}>
