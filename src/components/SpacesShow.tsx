5667e174cbb64698df0a1c33b92a191f019ea1bb
alfonsoaranzazu <alfonso@viruett.com>
2022-11-18 03:30:04 -0800

    mobile styling spaces

--- src/components/SpacesShow.tsx
+++ src/components/SpacesShow.tsx
@@ -19,23 +19,23 @@ export default function SpacesShow(props: SpacesShowProps) {
   const isSm = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
-    <Stack direction='row' spacing={8} alignItems='center' pb={5}>
+    <Stack direction={isSm ? 'column' : 'row'} spacing={4} justifyContent='center' alignItems='center' pb={5}>
       <Stack id='pfp' alignItems='center' spacing={2}>
         <Avatar alt={props.name} src={props.avatar} sx={{ width: 80, height: 80 }}/>
         <Link href={props.twitterUrl} target="_blank" color='secondary'><Twitter/></Link>
       </Stack>
-      <Stack direction={isMd ? 'column' : 'row'} spacing={isSm ? 2 : 8} alignItems={isSm ? 'start' : 'center'} pb={2} minWidth={300}>
-        <Stack id='titles'>
+      <Stack direction={isMd ? 'column' : 'row'} spacing={isSm ? 2 : 8} pb={2} minWidth={300}>
+        <Stack id='titles' alignItems='center'>
           <Typography variant='h4'>{props.name}</Typography>
           <Typography variant='subtitle1'>Hosted by {props.hostName}</Typography>
-          <Stack id='times' direction={isSm ? 'column' : 'row'} pt={2} spacing={2} width='fit-content'>
+          <Stack id='times' direction='row' pt={2} spacing={2} width='fit-content'>
             <Chip label={props.pst}/>
             <Chip label={props.est}/>
             <Chip label={props.gmt}/>
           </Stack>
         </Stack>
       </Stack>
-      <Stack id='banner' alignItems='center' width='100%' sx={{display: isLg ? 'none' : 'default'}}>
+      <Stack id='banner' alignItems='center' width='100%' sx={{display: isMd ? 'none' : 'default'}}>
         <Box component='img' src={props.bannerUrl} height={250} minWidth={500}/>
       </Stack>
     </Stack>
