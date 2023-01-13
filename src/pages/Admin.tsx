2f954b4f73a6c99bbfbeab017db31b0cf416aded
alfonsoaranzazu <alfonso@viruett.com>
2023-01-12 23:53:59 -0800

    adds admin tasks

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Paper, TextField, Theme, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Container, FormControl, FormControlLabel, FormLabel, Grid, Paper, Radio, RadioGroup, Stack, TextField, Theme, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { makeStyles, createStyles } from '@mui/styles';
 import { ChangeEvent, useState } from "react";
 import { useFroggiesOwned, useStakingDeposits } from "../client";
@@ -22,10 +22,15 @@ export default function Admin() {
   const deposits = useStakingDeposits(stakingWallet);
   const owned = useFroggiesOwned(stakingWallet);
   const isSm = useMediaQuery(theme.breakpoints.down('md'));
+  const [task, setTask] = useState('wallet');
 
   const onStakingWalletChange = (event: ChangeEvent<HTMLInputElement>) => {
     setStakingWallet(event.target.value);
   }
+
+  const onTaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTask((event.target as HTMLInputElement).value);
+  };
 
   const getRole = (deposits: number[]) => {
     const { length } = deposits;
@@ -48,6 +53,24 @@ export default function Admin() {
         <Grid id='banner' className={classes.banner} container height={isSm ? 300 : 600}/>
       </Paper>
       <Container maxWidth='xl'  sx={{pt: 5, pb: 5}}>
+        <Grid container justifyContent='space-between'>
+          <Stack>
+            <FormControl>
+              <FormLabel id="demo-controlled-radio-buttons-group">Admin Tasks</FormLabel>
+              <RadioGroup
+                aria-labelledby="demo-controlled-radio-buttons-group"
+                name="controlled-radio-buttons-group"
+                value={task}
+                onChange={onTaskChange}
+              >
+                <FormControlLabel value="wallet" control={<Radio />} label="Wallet Checker" />
+                <FormControlLabel value="list" control={<Radio />} label="List Item" />
+                <FormControlLabel value="update" control={<Radio />} label="Update Item" />
+              </RadioGroup>
+            </FormControl>
+          </Stack>
+        </Grid>
+        
         <Grid id="wallet" justifyContent="center" container pb={5}>
           <Grid item xl={4} lg={4} md={6} sm={8} xs={10}>
             <TextField label="Wallet" variant="outlined" fullWidth value={stakingWallet} onChange={onStakingWalletChange}/>
