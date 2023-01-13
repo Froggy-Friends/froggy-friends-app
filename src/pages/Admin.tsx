3ddac8ffd50447c33bc7f42cd97fe1e717e1f04e
alfonsoaranzazu <alfonso@viruett.com>
2023-01-12 23:45:23 -0800

    adds admin banner

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,11 +1,27 @@
-import { Grid, TextField, Typography } from "@mui/material";
+import { Container, Grid, Paper, TextField, Theme, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { makeStyles, createStyles } from '@mui/styles';
 import { ChangeEvent, useState } from "react";
 import { useFroggiesOwned, useStakingDeposits } from "../client";
+import banner from '../images/lab.jpg';
+import theme from "../theme";
+
+const useStyles: any = makeStyles((theme: Theme) => 
+  createStyles({
+    banner: {
+      backgroundImage: `url(${banner})`, 
+      backgroundSize: 'cover', 
+      backgroundPosition: 'center'
+    }
+  })
+);
 
 export default function Admin() {
+  const classes = useStyles();
+  const theme = useTheme();
   const [stakingWallet, setStakingWallet] = useState("");
   const deposits = useStakingDeposits(stakingWallet);
   const owned = useFroggiesOwned(stakingWallet);
+  const isSm = useMediaQuery(theme.breakpoints.down('md'));
 
   const onStakingWalletChange = (event: ChangeEvent<HTMLInputElement>) => {
     setStakingWallet(event.target.value);
@@ -27,32 +43,34 @@ export default function Admin() {
   }
 
   return (
-    <Grid id="admin" container alignItems="center" direction="column" pt={20} pb={30}>
-      <Typography variant="h3" pb={10}>Staking Deposits Checker </Typography>
-
-      <Grid id="wallet" justifyContent="center" container pb={5}>
-        <Grid item xl={4} lg={4} md={6} sm={8} xs={10}>
-          <TextField label="Wallet" variant="outlined" fullWidth value={stakingWallet} onChange={onStakingWalletChange}/>
+    <Grid id="admin" className={classes.market} container direction="column" justifyContent="start">
+      <Paper elevation={3}>
+        <Grid id='banner' className={classes.banner} container height={isSm ? 300 : 600}/>
+      </Paper>
+      <Container maxWidth='xl'  sx={{pt: 5, pb: 5}}>
+        <Grid id="wallet" justifyContent="center" container pb={5}>
+          <Grid item xl={4} lg={4} md={6} sm={8} xs={10}>
+            <TextField label="Wallet" variant="outlined" fullWidth value={stakingWallet} onChange={onStakingWalletChange}/>
+          </Grid>
         </Grid>
-      </Grid>
-
-      {
-        deposits &&
-        <Grid id="staked" pb={5}>
-          <Grid item>
-            <Typography variant="h5">Froggies Unstaked: {owned}</Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="h5">Froggies Staked: {deposits.length}</Typography>
-          </Grid>
-          <Grid item>
-          <Typography variant="h5">Froggies Owned: {owned + deposits.length}</Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="h5">Whale Roles: {getRole(deposits)}</Typography>
-          </Grid>
-        </Grid> 
-      }
+        {
+          deposits &&
+          <Grid id="staked" pb={5}>
+            <Grid item>
+              <Typography variant="h5">Froggies Unstaked: {owned}</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="h5">Froggies Staked: {deposits.length}</Typography>
+            </Grid>
+            <Grid item>
+            <Typography variant="h5">Froggies Owned: {owned + deposits.length}</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="h5">Whale Roles: {getRole(deposits)}</Typography>
+            </Grid>
+          </Grid> 
+        }
+      </Container>
     </Grid>
   )
 }
