d29b0f06004b58a75f9827d6e87f5153861a150c
alfonsoaranzazu <alfonso@viruett.com>
2023-01-13 01:19:42 -0800

    fetch item presets

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,8 +1,10 @@
-import { Container, FormControl, FormControlLabel, FormLabel, Grid, Paper, Radio, RadioGroup, Stack, TextField, Theme, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Container, FormControl, FormControlLabel, FormLabel, Grid, InputLabel, MenuItem, Paper, Radio, RadioGroup, Select, SelectChangeEvent, Stack, Switch, TextField, Theme, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { makeStyles, createStyles } from '@mui/styles';
-import { ChangeEvent, useState } from "react";
+import axios from "axios";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useFroggiesOwned, useStakingDeposits } from "../client";
 import banner from '../images/lab.jpg';
+import { ItemPresets } from "../models/ItemPresets";
 import theme from "../theme";
 
 const useStyles: any = makeStyles((theme: Theme) => 
@@ -22,7 +24,26 @@ export default function Admin() {
   const deposits = useStakingDeposits(stakingWallet);
   const owned = useFroggiesOwned(stakingWallet);
   const isSm = useMediaQuery(theme.breakpoints.down('md'));
-  const [task, setTask] = useState('wallet');
+  const [task, setTask] = useState('list');
+  const [presets, setPresets] = useState<ItemPresets>();
+  const [itemName, setItemName] = useState('');
+  const [itemOnSale, setItemOnSale] = useState(false);
+  const [itemCategory, setItemCategory] = useState('');
+  const [itemFriendOrigin, setItemFriendOrigin] = useState('');
+
+  useEffect(() => {
+    async function getPresets() {
+      try {
+        const response = await axios.get<ItemPresets>(`${process.env.REACT_APP_API}/items/presets`);
+        const fetchedPresets = response.data;
+        setPresets(fetchedPresets);
+      } catch (error) {
+        console.log("error fetching item presets: ", error);
+      }
+    }
+
+    getPresets();
+  }, [])
 
   const onStakingWalletChange = (event: ChangeEvent<HTMLInputElement>) => {
     setStakingWallet(event.target.value);
@@ -47,13 +68,29 @@ export default function Admin() {
     }
   }
 
+  const onItemNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setItemName(event.target.value);
+  }
+
+  const onItemSaleChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setItemOnSale(event.target.checked);
+  };
+
+  const onItemCategoryChange = (event: SelectChangeEvent) => {
+    setItemCategory(event.target.value as string);
+  }
+
+  const onItemFriendOriginChange = (event: SelectChangeEvent) => {
+    setItemFriendOrigin(event.target.value as string);
+  }
+
   return (
     <Grid id="admin" className={classes.market} container direction="column" justifyContent="start">
       <Paper elevation={3}>
         <Grid id='banner' className={classes.banner} container height={isSm ? 300 : 600}/>
       </Paper>
       <Container maxWidth='xl'  sx={{pt: 15, pb: 25}}>
-        <Grid container justifyContent='space-between'>
+        <Grid container spacing={5}>
           <Stack>
             <FormControl>
               <FormLabel id="demo-controlled-radio-buttons-group">Admin Tasks</FormLabel>
@@ -92,6 +129,61 @@ export default function Admin() {
                   </Grid>
                 </Grid> 
               }
+            </Stack>
+          }
+          {
+            task === 'list' &&
+            <Stack>
+              <FormControl component='fieldset' variant='standard'>
+                <Stack direction='row'>
+                  <TextField label="Name" variant="outlined" value='itemName' onChange={onItemNameChange}/>
+                  <FormControlLabel
+                    control={
+                      <Switch checked={itemOnSale} onChange={onItemSaleChanged}/>
+                    }
+                    label='On Sale'
+                    labelPlacement="top"
+                  />
+                </Stack>
+                <Stack direction='row' spacing={2}>
+                  <Stack minWidth={100}>
+                    <FormControl fullWidth>
+                      <InputLabel id="category-label">Category</InputLabel>
+                      <Select
+                        labelId="category-label"
+                        id="category"
+                        value={itemCategory}
+                        label="Category"
+                        onChange={onItemCategoryChange}
+                      >
+                        {
+                          presets?.categories.map((category, index) => (
+                            <MenuItem key={index} value={category}>{category}</MenuItem>
+                          ))
+                        }
+                      </Select>
+                    </FormControl>
+                  </Stack>
+                  <Stack minWidth={100}>
+                    <FormControl fullWidth>
+                      <InputLabel id="friendOrigin-label">Friend Origin</InputLabel>
+                      <Select
+                        labelId="friendOrigin-label"
+                        id="friendOrigin"
+                        value={itemFriendOrigin}
+                        label="friendOrigin"
+                        onChange={onItemFriendOriginChange}
+                      >
+                        {
+                          presets?.friendOrigins.map((friendOrigin, index) => (
+                            <MenuItem key={index} value={friendOrigin}>{friendOrigin}</MenuItem>
+                          ))
+                        }
+                      </Select>
+                    </FormControl>
+                  </Stack>
+                </Stack>
+              </FormControl>
             </Stack>
           }
         </Grid>
