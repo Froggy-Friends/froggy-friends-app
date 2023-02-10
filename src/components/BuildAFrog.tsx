b5c8e5503b84a1091f7b27871023eca902914c2b
alfonsoaranzazu <alfonso@viruett.com>
2023-02-10 07:36:55 -0800

    adds body and eyes

--- src/components/BuildAFrog.tsx
+++ src/components/BuildAFrog.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, Fragment, useEffect, useState } from "react";
 import { Card, CardMedia, Grid, Stack, TextField, Typography } from "@mui/material";
 import { Trait } from "../models/Trait";
 import axios from "axios";
+import mergeImages from 'merge-images';
 
 export default function BuildAFrog() {
   const [preview, setPreview] = useState('');
@@ -12,10 +13,21 @@ export default function BuildAFrog() {
   const [mouthTraits, setMouthTraits] = useState<Trait[]>([]);
   const [shirtTraits, setShirtTraits] = useState<Trait[]>([]);
   const [hatTraits, setHatTraits] = useState<Trait[]>([]);
+  const [selectedBackground, setSelectedBackground] = useState<Trait>();
+  const [selectedBody, setSelectedBody] = useState<Trait>();
+  const [selectedEyes, setSelectedEyes] = useState<Trait>();
 
   useEffect(() => {
     loadTraits();
   }, [])
+
+  useEffect(() => {
+    loadPreview(
+      selectedBackground,
+      selectedBody,
+      selectedEyes
+    );
+  }, [selectedBackground, selectedBody, selectedEyes]);
 
   const loadTraits = async () => {
     try {
@@ -36,21 +48,74 @@ export default function BuildAFrog() {
     }
   }
 
+  const loadPreview = async (bg: Trait | undefined, body: Trait | undefined, eyes: Trait | undefined) => {
+    const sources = [];
+
+    if (bg) sources.push(bg.imageTransparent);
+    if (body) sources.push(body.imageTransparent);
+    if (eyes) sources.push(eyes.imageTransparent);
+
+    if (sources.length) {
+      const prev = await mergeImages(sources, { crossOrigin: 'anonymous', width: 2000, height: 2000});
+      setPreview(prev);
+    }
+  }
+
+  const onBackgroundClicked = (trait: Trait) => {
+    setSelectedBackground(trait);
+  }
+
+  const onBodyClick = (trait: Trait) => {
+    setSelectedBody(trait);
+  }
+
+  const onEyesClick = (trait: Trait) => {
+    setSelectedEyes(trait);
+  }
+
   return (
     <Stack id='build-a-frog' direction='row' spacing={5}>
       <Stack id='preview'>
-        <Typography variant='h6'>Build-A-Frog</Typography>
+        <Typography variant='h5' pb={2}>Build-A-Frog</Typography>
         { preview && <img src={preview} alt='' height={400} width={400} style={{backgroundColor: 'white'}}/> }
         { !preview && <Stack width={400} height={400} bgcolor='white'/>}
       </Stack>
       <Stack id='traits' spacing={2} p={2}>
-        <Typography p={2}>Backgrounds</Typography>
+        <Typography variant='h6' pl={2}>Backgrounds</Typography>
         <Grid id='backgrounds' container>
           {
-            backgroundTraits.map(trait => {
+            backgroundTraits.map((trait, index) => {
               return (
-                <Grid item key={trait.id} xl={3} lg={3} md={3} sm={3} xs={3} p={2}>
-                  <Card>
+                <Grid item key={index} xl={2} lg={2} md={2} sm={2} xs={2} p={2}>
+                  <Card onClick={() => onBackgroundClicked(trait)}>
+                    <CardMedia component='img' src={trait.imageTransparent} height={100} width={100} alt='' sx={{backgroundColor: '#93d0aa'}}/>
+                  </Card>
+                </Grid>
+              )
+            })
+          }
+        </Grid>
+        <Typography variant='h6' pl={2}>Body</Typography>
+        <Grid id='body' container>
+          {
+            bodyTraits.map((trait, index) => {
+              return (
+                <Grid item key={index} xl={2} lg={2} md={2} sm={2} xs={2} p={2}>
+                  <Card onClick={() => onBodyClick(trait)}>
+                    <CardMedia component='img' src={trait.imageTransparent} height={100} width={100} alt='' sx={{backgroundColor: '#93d0aa'}}/>
+                  </Card>
+                </Grid>
+              )
+            })
+          }
+        </Grid>
+        <Typography variant='h6' pl={2}>Eyes</Typography>
+        <Grid id='eyes' container>
+          {
+            eyeTraits.map((trait, index) => {
+              return (
+                <Grid item key={index} xl={2} lg={2} md={2} sm={2} xs={2} p={2}>
+                  <Card onClick={() => onEyesClick(trait)}>
                     <CardMedia component='img' src={trait.imageTransparent} height={100} width={100} alt='' sx={{backgroundColor: '#93d0aa'}}/>
                   </Card>
                 </Grid>
